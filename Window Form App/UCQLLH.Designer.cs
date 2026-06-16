b341248f29d2df04567f18f4ef994298b2c2994a
Kencd58 <kienkienkien0508@gmail.com>
2026-06-16 15:45:41 +0700

    feat<sinhvien>: SearchPaging

---
+++ Window Form App/UCQLLH.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Window_Form_App
+{
+    partial class UCQLLH
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.page = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.timKiem_btn = new System.Windows.Forms.Button();
+            this.txt_find = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.refresh_btn = new System.Windows.Forms.Button();
+            this.delete_btn = new System.Windows.Forms.Button();
+            this.sua_btn = new System.Windows.Forms.Button();
+            this.add_btn = new System.Windows.Forms.Button();
+            this.txt_tenLop = new System.Windows.Forms.TextBox();
+            this.txt_lop = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_dssv = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // page
+            // 
+            this.page.AutoSize = true;
+            this.page.Location = new System.Drawing.Point(641, 487);
+            this.page.Name = "page";
+            this.page.Size = new System.Drawing.Size(64, 16);
+            this.page.TabIndex = 23;
+            this.page.Text = "Trang 1/1";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(812, 484);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 22;
+            this.button9.Text = "> >";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(731, 484);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 21;
+            this.button8.Text = ">";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(439, 484);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "< <";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(520, 484);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "<";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // timKiem_btn
+            // 
+            this.timKiem_btn.Location = new System.Drawing.Point(788, 36);
+            this.timKiem_btn.Name = "timKiem_btn";
+            this.timKiem_btn.Size = new System.Drawing.Size(75, 23);
+            this.timKiem_btn.TabIndex = 18;
+            this.timKiem_btn.Text = "Tìm kiếm";
+            this.timKiem_btn.UseVisualStyleBackColor = true;
+            // 
+            // txt_find
+            // 
+            this.txt_find.Location = new System.Drawing.Point(480, 37);
+            this.txt_find.Name = "txt_find";
+            this.txt_find.Size = new System.Drawing.Size(262, 22);
+            this.txt_find.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(339, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 16);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Tìm kiếm theo mã lớp";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(342, 78);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(698, 353);
+            this.dataGridView2.TabIndex = 15;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_dssv);
+            this.groupBox1.Controls.Add(this.refresh_btn);
+            this.groupBox1.Controls.Add(this.delete_btn);
+            this.groupBox1.Controls.Add(this.sua_btn);
+            this.groupBox1.Controls.Add(this.add_btn);
+            this.groupBox1.Controls.Add(this.txt_tenLop);
+            this.groupBox1.Controls.Add(this.txt_lop);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(44, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 518);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin lớp học";
+            // 
+            // refresh_btn
+            // 
+            this.refresh_btn.Location = new System.Drawing.Point(114, 417);
+            this.refresh_btn.Name = "refresh_btn";
+            this.refresh_btn.Size = new System.Drawing.Size(75, 23);
+            this.refresh_btn.TabIndex = 14;
+            this.refresh_btn.Text = "Làm mới";
+            this.refresh_btn.UseVisualStyleBackColor = true;
+            // 
+            // delete_btn
+            // 
+            this.delete_btn.Location = new System.Drawing.Point(21, 417);
+            this.delete_btn.Name = "delete_btn";
+            this.delete_btn.Size = new System.Drawing.Size(75, 23);
+            this.delete_btn.TabIndex = 13;
+            this.delete_btn.Text = "Xóa";
+            this.delete_btn.UseVisualStyleBackColor = true;
+            // 
+            // sua_btn
+            // 
+            this.sua_btn.Location = new System.Drawing.Point(114, 368);
+            this.sua_btn.Name = "sua_btn";
+            this.sua_btn.Size = new System.Drawing.Size(75, 23);
+            this.sua_btn.TabIndex = 12;
+            this.sua_btn.Text = "Sửa";
+            this.sua_btn.UseVisualStyleBackColor = true;
+            // 
+            // add_btn
+            // 
+            this.add_btn.Location = new System.Drawing.Point(21, 368);
+            this.add_btn.Name = "add_btn";
+            this.add_btn.Size = new System.Drawing.Size(75, 23);
+            this.add_btn.TabIndex = 11;
+            this.add_btn.Text = "Thêm";
+            this.add_btn.UseVisualStyleBackColor = true;
+            // 
+            // txt_tenLop
+            // 
+            this.txt_tenLop.Location = new System.Drawing.Point(69, 166);
+            this.txt_tenLop.Name = "txt_tenLop";
+            this.txt_tenLop.Size = new System.Drawing.Size(100, 22);
+            this.txt_tenLop.TabIndex = 8;
+            // 
+            // txt_lop
+            // 
+            this.txt_lop.Location = new System.Drawing.Point(68, 112);
+            this.txt_lop.Name = "txt_lop";
+            this.txt_lop.Size = new System.Drawing.Size(100, 22);
+            this.txt_lop.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tên lớp";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mã lớp";
+            // 
+            // btn_dssv
+            // 
+            this.btn_dssv.Location = new System.Drawing.Point(6, 462);
+            this.btn_dssv.Name = "btn_dssv";
+            this.btn_dssv.Size = new System.Drawing.Size(201, 23);
+            this.btn_dssv.TabIndex = 15;
+            this.btn_dssv.Text = "Xem danh sách sinh viên";
+            this.btn_dssv.UseVisualStyleBackColor = true;
+            // 
+            // UCQLLH
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.page);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.timKiem_btn);
+            this.Controls.Add(this.txt_find);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "UCQLLH";
+            this.Size = new System.Drawing.Size(1495, 823);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label page;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button timKiem_btn;
+        private System.Windows.Forms.TextBox txt_find;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button refresh_btn;
+        private System.Windows.Forms.Button delete_btn;
+        private System.Windows.Forms.Button sua_btn;
+        private System.Windows.Forms.Button add_btn;
+        private System.Windows.Forms.TextBox txt_tenLop;
+        private System.Windows.Forms.TextBox txt_lop;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_dssv;
+    }
+}
--- Window Form App/UCQLSV.cs
+++ Window Form App/UCQLSV.cs
@@ -12,12 +12,59 @@ namespace Window_Form_App
 {
 
     public partial class UCQLSV : Form
+
     {
+        int currentPage = 1;
+        int pageSize = 5;
+        int totalPage = 1;
         private void LoadData()
         {
             DataClasses1DataContext db = new DataClasses1DataContext();
-            List<sinh_vien> dssv = db.sinh_viens.ToList();
+
+            int totalRecord = db.sinh_viens.Count();
+
+            totalPage = (int)Math.Ceiling((double)totalRecord / pageSize);
+
+            if (totalPage == 0)
+            {
+                totalPage = 1;
+            }
+
+            if (currentPage > totalPage)
+            {
+                currentPage = totalPage;
+            }
+
+            if (currentPage < 1)
+            {
+                currentPage = 1;
+            }
+
+            List<sinh_vien> dssv = db.sinh_viens
+                .OrderBy(sv => sv.ma_sv)
+                .Skip((currentPage - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
             dgvSinhVien.DataSource = dssv;
+
+            lbPage.Text = "Trang " + currentPage + "/" + totalPage + " | " + totalRecord + " bản ghi";
+        }
+        private void LoadLop()
+        {
+            DataClasses1DataContext db = new DataClasses1DataContext();
+
+            List<lop_hoc> dsLop = db.lop_hocs.ToList();
+
+            cbLop.DataSource = dsLop;
+            cbLop.DisplayMember = "ten_lop";
+            cbLop.ValueMember = "ma_lop";
+        }
+        private void UCQLSV_Load(object sender, EventArgs e)
+        {
+            LoadLop();
+            LoadData();
+
         }
 
         public UCQLSV()
@@ -29,7 +76,7 @@ namespace Window_Form_App
             colHoTen.DataPropertyName = "ho_ten";
             colGioiTinh.DataPropertyName = "gioi_tinh";
             colNgaySinh.DataPropertyName = "ngay_sinh";
-            //colLop.DataPropertyName = "ma_lop";
+            colLop.DataPropertyName = "ma_lop";
             cbGioiTinh.Items.Add("Nam");
             cbGioiTinh.Items.Add("Nữ");
             cbGioiTinh.SelectedIndex = 0;
@@ -38,11 +85,24 @@ namespace Window_Form_App
             btnXoa.Click += btnXoa_Click;
 
             this.Load += UCQLSV_Load;
+
+            btnFirstPage.Click += btnFirstPage_Click;
+            btnPreviousPage.Click += btnPreviousPage_Click;
+            btnNextPage.Click += btnNextPage_Click;
+            btnLastPage.Click += btnLastPage_Click;
+
+
+
+            button2.Click += button2_Click; 
         }
 
-        private void UCQLSV_Load(object sender, EventArgs e)
+
+        private void button2_Click(object sender, EventArgs e)
         {
-            LoadData();
+            UCQLLH formLop = new UCQLLH();
+            formLop.Show();
+
+            this.Hide();
         }
 
         private void btnThem_Click(object sender, EventArgs e)
@@ -51,7 +111,7 @@ namespace Window_Form_App
             string MaSinhVien = txtMSV.Text;
             DateTime NgaySinh = dtpNgaySinh.Value;
             string GioiTinh = cbGioiTinh.Text;
-            //string Lop = txt_lop.Text;
+            string Lop = cbLop.SelectedValue.ToString();
 
             sinh_vien sv = new sinh_vien();
 
@@ -59,7 +119,7 @@ namespace Window_Form_App
             sv.ho_ten = HoVaTen;
             sv.ngay_sinh = NgaySinh;
             sv.gioi_tinh = GioiTinh;
-            //sv.ma_lop = Lop;
+            sv.ma_lop = Lop;
 
             DataClasses1DataContext db = new DataClasses1DataContext();
 
@@ -82,6 +142,7 @@ namespace Window_Form_App
             txtMSV.Text = row.Cells["colMaSV"].Value?.ToString();
             txtHoTen.Text = row.Cells["colHoTen"].Value?.ToString();
             cbGioiTinh.Text = row.Cells["colGioiTinh"].Value?.ToString();
+            cbLop.SelectedValue = row.Cells["colLop"].Value?.ToString();
 
             if (row.Cells["colNgaySinh"].Value != null)
             {
@@ -99,6 +160,7 @@ namespace Window_Form_App
                 sv.ho_ten = txtHoTen.Text;
                 sv.gioi_tinh = cbGioiTinh.Text;
                 sv.ngay_sinh = dtpNgaySinh.Value;
+                sv.ma_lop = cbLop.SelectedValue.ToString();
                 db.SubmitChanges();
                 MessageBox.Show("Cập nhật sinh viên thành công!");
                 LoadData();
@@ -125,5 +187,31 @@ namespace Window_Form_App
                 MessageBox.Show("Không tìm thấy sinh viên với mã đã nhập.");
             }
         }
-}
+        private void btnFirstPage_Click(object sender, EventArgs e)
+        {
+            currentPage = 1;
+            LoadData();
+        }
+        private void btnPreviousPage_Click(object sender, EventArgs e)
+        {
+            if (currentPage > 1)
+            {
+                currentPage--;
+                LoadData();
+            }
+        }
+        private void btnNextPage_Click(object sender, EventArgs e)
+        {
+            if (currentPage < totalPage)
+            {
+                currentPage++;
+                LoadData();
+            }
+        }
+        private void btnLastPage_Click(object sender, EventArgs e)
+        {
+            currentPage = totalPage;
+            LoadData();
+        }
+    }
 }
